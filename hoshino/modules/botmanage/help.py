968dab1278bbfc115490e7a8b24d16aaa9a84c38
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-07-06 11:28:15 +0800

    Create help.py

---
+++ hoshino/modules/botmanage/help.py
@@ -0,0 +1,36 @@
+from hoshino import Service, priv
+from hoshino.typing import CQEvent
+
+sv = Service('_help_', manage_priv=priv.SUPERUSER, visible=False)
+
+TOP_MANUAL = '''
+发送方括号[]内的关键词即可触发
+
+[怎么拆日和] 竞技场查询
+[优妮来发十连] 转蛋模拟
+[切噜一下] 切噜语转换
+[谁是霸瞳] 别名查询
+'''.strip()
+
+def gen_bundle_manual(bundle_name, service_list, gid):
+    manual = [bundle_name]
+    service_list = sorted(service_list, key=lambda s: s.name)
+    for sv in service_list:
+        if sv.visible:
+            spit_line = '=' * max(0, 18 - len(sv.name))
+            manual.append(f"|{'○' if sv.check_enabled(gid) else '×'}| {sv.name} {spit_line}")
+            if sv.help:
+                manual.append(sv.help)
+    return '\n'.join(manual)
+
+
+@sv.on_prefix(('help', '帮助', '幫助'))
+async def send_help(bot, ev: CQEvent):
+    bundle_name = ev.message.extract_plain_text().strip()
+    bundles = Service.get_bundles()
+    if not bundle_name:
+        await bot.send(ev, TOP_MANUAL)
+    elif bundle_name in bundles:
+        msg = gen_bundle_manual(bundle_name, bundles[bundle_name], ev.group_id)
+        await bot.send(ev, msg)
+    # else: ignore
