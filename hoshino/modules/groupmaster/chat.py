6ce2cdaa89f1cfd2464dd29a9b14b7fbe775d2eb
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-07-06 11:29:04 +0800

    Create chat.py

---
+++ hoshino/modules/groupmaster/chat.py
@@ -0,0 +1,85 @@
+import random
+
+from nonebot import on_command
+
+from hoshino import R, Service, priv, util
+
+
+# basic function for debug, not included in Service('chat')
+@on_command('zai?', aliases=('在?', '在？', '在吗', '在么？', '在嘛', '在嘛？'), only_to_me=True)
+async def say_hello(session):
+    await session.send('在啊')
+
+
+sv = Service('chat', visible=False)
+
+@sv.on_keyword(('沙雕机器人', '沙雕機器人'))
+async def say_sorry(bot, ev):
+    await bot.send(ev, '你在说谁？')
+
+
+@sv.on_fullmatch(('老婆', 'waifu', 'laopo'), only_to_me=True)
+async def chat_waifu(bot, ev):
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        await bot.send(ev, R.img('laopo.jpg').cqcode)
+    else:
+        await bot.send(ev, 'mua~')
+
+
+@sv.on_fullmatch('老公', only_to_me=True)
+async def chat_laogong(bot, ev):
+    await bot.send(ev, '人不能，至少不应该。', at_sender=True)
+
+
+@sv.on_fullmatch('mua', only_to_me=True)
+async def chat_mua(bot, ev):
+    await bot.send(ev, '笨蛋~', at_sender=True)
+
+'''
+@sv.on_fullmatch('来点星奏')
+async def seina(bot, ev):
+    await bot.send(ev, R.img('星奏.png').cqcode)
+
+
+@sv.on_fullmatch(('我有个朋友说他好了', '我朋友说他好了', ))
+async def ddhaole(bot, ev):
+    await bot.send(ev, '那个朋友是不是你弟弟？')
+    await util.silence(ev, 30)
+'''
+
+@sv.on_fullmatch('我好了')
+async def nihaole(bot, ev):
+    await bot.send(ev, '不许好，憋回去！')
+    await util.silence(ev, 30)
+
+
+
+# ============================================ #
+
+
+@sv.on_keyword(('确实', '有一说一', 'u1s1', 'yysy'))
+async def chat_queshi(bot, ctx):
+    if random.random() < 0.1:
+        await bot.send(ctx, R.img('确实.jpg').cqcode)
+
+
+@sv.on_keyword(('会战'))
+async def chat_clanba(bot, ctx):
+    if random.random() < 0.04:
+        await bot.send(ctx, R.img('我的天啊你看看都几点了.jpg').cqcode)
+
+
+@sv.on_keyword(('内鬼'))
+async def chat_neigui(bot, ctx):
+    if random.random() < 0.2:
+        await bot.send(ctx, R.img('内鬼.png').cqcode)
+
+nyb_player = f'''正在播放：New Year Burst
+──●━━━━ 1:05/1:30
+⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
+'''.strip()
+
+@sv.on_keyword(('春黑', '新黑'))
+async def new_year_burst(bot, ev):
+    if random.random() < 0.05:
+        await bot.send(ev, nyb_player)
