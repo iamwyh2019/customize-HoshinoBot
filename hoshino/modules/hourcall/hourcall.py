fb546f25411f4b8c9099950ce4a71b63b1fd65a2
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-07-28 22:16:55 +0800

    公会战期间特别报时
    在公会战期间用一套特别的报时文案。同时增加手动触发报时功能，方便调试

--- hoshino/modules/hourcall/hourcall.py
+++ hoshino/modules/hourcall/hourcall.py
@@ -2,6 +2,8 @@ import pytz
 from datetime import datetime
 import hoshino
 from hoshino import Service
+from hoshino.service import sucmd
+from hoshino.typing import CommandSession
 
 sv = Service('hourcall', enable_on_default=False, help_='时报')
 tz = pytz.timezone('Asia/Shanghai')
@@ -10,6 +12,13 @@ def get_hour_call():
     """挑出一组时报，每日更换，一日之内保持相同"""
     cfg = hoshino.config.hourcall
     now = datetime.now(tz)
+
+    st,en = cfg.CLAN_BATTLE_DATE
+    st = datetime.strptime(st,"%Y/%m/%d %H:%M:%S").replace(tzinfo=tz)
+    en = datetime.strptime(en,"%Y/%m/%d %H:%M:%S").replace(tzinfo=tz)
+    if st<=now<=en:
+        return cfg.HOUR_CALLS["HOUR_CALL_CLANBATTLE"]
+        
     hc_groups = cfg.HOUR_CALLS_ON
     g = hc_groups[ now.day % len(hc_groups) ]
     return cfg.HOUR_CALLS[g]
@@ -22,3 +31,11 @@ async def hour_call():
         return  # 宵禁 免打扰
     msg = get_hour_call()[now.hour]
     await sv.broadcast(msg, 'hourcall', 0)
+
+
+@sucmd('manualhourcall', aliases=('mhc', '手动报时'))
+async def manual_hourcall(session: CommandSession):
+    msg = session.current_arg
+    if msg!="":
+        await sv.broadcast(msg, 'hourcall', 0)
+    await hour_call()
