48b88c87435a8b16a9b0dff7a9e34ffa03b2c514
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-08-11 18:44:12 +0800

    编写了空调插件

---
+++ hoshino/modules/aircon/__init__.py
@@ -0,0 +1,157 @@
+import datetime
+from .airconutils import get_group_aircon, write_group_aircon, update_aircon, new_aircon, print_aircon
+from hoshino import Service
+
+try:
+	import ujson as json
+except:
+	import json
+
+sv = Service('aircon', visible=True)
+# initialize
+
+aircons = get_group_aircon(__file__)
+
+async def check_status(gid,bot,event):
+
+	if gid not in aircons:
+		await bot.send(event, "空调还没装哦~发送“开空调”安装空调")
+		return None
+
+	aircon = aircons[gid]
+	if not aircon["is_on"]:
+		await bot.send(event,"你空调没开！")
+		return None
+
+	return aircon
+
+async def check_range(bot,event,low,high,errormsg):
+
+	msg = event.message.extract_plain_text().split()
+	try:
+		val = int(msg[0])
+	except:
+		await bot.send(event, "输入有误！")
+		return None
+
+	if not low<=val<=high:
+		await bot.send(event,errormsg)
+		return None
+
+	return val
+
+@sv.on_fullmatch('开空调')
+async def aircon_on(bot,event):
+
+	gid = str(event['group_id'])
+
+	if gid not in aircons:
+		ginfo = await bot.get_group_info(group_id = gid)
+		gcount = ginfo["member_count"]
+		aircon = new_aircon(num_member = gcount)
+		aircons[gid] = aircon
+		await bot.send(event,"❄空调已安装~")
+	else:
+		aircon = aircons[gid]
+
+	update_aircon(aircon)
+	msg = print_aircon(aircon)
+	aircon['is_on'] = True
+	write_group_aircon(__file__,aircons)
+	await bot.send(event, '❄哔~空调已开\n' + msg)
+
+@sv.on_fullmatch('关空调')
+async def aircon_off(bot,event):
+
+	gid = str(event['group_id'])
+
+	aircon = await check_status(gid,bot,event)
+	if aircon is None:
+		return
+
+	update_aircon(aircon)
+	msg = print_aircon(aircon)
+	aircon['is_on'] = False
+	write_group_aircon(__file__,aircons)
+	await bot.send(event, '❄哔~空调已关\n' + msg)
+
+@sv.on_fullmatch('当前温度')
+async def aircon_now(bot,event):
+
+	gid = str(event['group_id'])
+
+	if gid not in aircons:
+		await bot.send(event, "空调还没装哦~发送“开空调”安装空调")
+		return
+
+	aircon = aircons[gid]
+	update_aircon(aircon)
+	msg = print_aircon(aircon)
+	write_group_aircon(__file__,aircons)
+	await bot.send(event, "❄"+msg)
+
+@sv.on_prefix(('设置温度','设定温度'))
+async def set_temp(bot,event):
+
+	gid = str(event['group_id'])
+
+	aircon = await check_status(gid,bot,event)
+	if aircon is None:
+		return
+
+	set_temp = await check_range(bot,event,0,999999,"只能设置0-999999°C喔")
+	if set_temp is None:
+		return
+
+	if set_temp == 114514:
+		await bot.send(event,"这么臭的空调有什么装的必要吗")
+		return
+
+	update_aircon(aircon)
+	aircon["set_temp"] = set_temp
+	msg = print_aircon(aircon)
+	write_group_aircon(__file__,aircons)
+	await bot.send(event,"❄"+msg)
+
+@sv.on_prefix(('设置风速','设定风速','设置风量','设定风量'))
+async def set_wind_rate(bot,event):
+
+	gid = str(event['group_id'])
+
+	aircon = await check_status(gid,bot,event)
+	if aircon is None:
+		return
+
+	wind_rate = await check_range(bot,event,0,999999,"只能设置1/2/3档喔")
+	if wind_rate is None:
+		return
+
+	update_aircon(aircon)
+	aircon["wind_rate"] = wind_rate - 1
+	msg = print_aircon(aircon)
+	write_group_aircon(__file__,aircons)
+	await bot.send(event,"❄"+msg)
+
+@sv.on_prefix(('设置环境温度','设定环境温度'))
+async def set_env_temp(bot,event):
+
+	gid = str(event['group_id'])
+
+	if gid not in aircons:
+		await bot.send(event, "空调还没装哦~发送“开空调”安装空调")
+		return
+
+	env_temp = await check_range(bot,event,0,999999,"只能设置0-999999°C喔")
+	if env_temp is None:
+		return
+
+	if env_temp == 114514:
+		await bot.send(event,"这么臭的空调有什么装的必要吗")
+		return
+
+	aircon = aircons[gid]
+	update_aircon(aircon)
+	aircon["env_temp"] = env_temp
+	msg = print_aircon(aircon)
+	write_group_aircon(__file__,aircons)
+	await bot.send(event,"❄"+msg)
---
+++ hoshino/modules/aircon/airconutils.py
@@ -0,0 +1,103 @@
+import datetime
+import os
+import hoshino
+
+try:
+	import ujson as json
+except:
+	import json
+
+R = 8.314 #理想气体常数
+i = 6 #多分子气体自由度
+K = 273 #开氏度
+vgas = 22.4 #气体摩尔体积
+unit_volume = 2 #每人体积
+aircon_off = 0.005 #关空调后每秒温度变化量
+ac_volume = [0.178, 0.213, 0.267] #每秒进风量
+powers = [5000, 6000, 7500] #功率
+volume_text = ["低","中","高"]
+
+def sgn(diff):
+	return 1 if diff>0 else -1 if diff<0 else 0
+
+def get_group_aircon(builtin_path):
+	filename = os.path.join(os.path.dirname(builtin_path), 'aircon.json')
+	try:
+		with open(filename, encoding='utf8') as f:
+			aircons = json.load(f)
+			return aircons
+	except Exception as e:
+		return {}
+
+def write_group_aircon(builtin_path, aircons):
+	filename = os.path.join(os.path.dirname(builtin_path), 'aircon.json')
+	with open(filename, 'w', encoding='utf8') as f:
+		json.dump(aircons, f, ensure_ascii=False)
+
+def new_aircon(num_member, set_temp = 26, now_temp = 33):
+	volume = max(num_member * unit_volume, 20)
+	return {"is_on":True, "env_temp": now_temp, "now_temp": now_temp, 
+			"set_temp": set_temp, "last_update": now_second(),
+			"volume": volume, "wind_rate": 0}
+
+def now_second():
+	return int((datetime.datetime.now()-datetime.datetime(1970,1,1)).total_seconds())
+
+def get_temp(N, n,setting, prev, T, power):
+	direction = sgn(setting-prev)
+	threshold = power / (n*1000/vgas) / (i/2) / R
+	cps = power / (N*1000/vgas) / (i/2) / R
+
+	if (abs(setting-prev)-threshold) >= cps * T:
+		new_temp = prev + direction * cps * T
+	else:
+		t1 = max(0,int((abs(setting-prev)-threshold)/cps))
+		temp1 = prev + direction * cps * t1
+		new_temp = (1-n/N)**(T-t1-1) * (temp1-setting) + setting
+	return round(new_temp,1)
+
+def update_aircon(aircon):
+
+	if aircon["is_on"]:
+		now_temp = aircon["now_temp"]
+		last_update = aircon["last_update"]
+		volume = aircon["volume"]
+		set_temp = aircon["set_temp"]
+		wind_rate = ac_volume[aircon["wind_rate"]]
+		power = powers[aircon["wind_rate"]]
+
+		new_update = now_second()
+		t_delta = new_update - last_update
+		new_temp = get_temp(volume, wind_rate, set_temp, now_temp, t_delta, power)
+
+		aircon["now_temp"] = new_temp
+		aircon["last_update"] = new_update
+
+	else:
+		env_temp = aircon["env_temp"]
+		now_temp = aircon["now_temp"]
+		last_update = aircon["last_update"]
+		new_update = now_second()
+		timedelta = new_update - last_update
+
+		direction = sgn(env_temp - now_temp)
+		new_temp = now_temp + direction * timedelta
+		if (env_temp-now_temp)*(env_temp-new_temp)<0: #过头了
+			new_temp = env_temp
+
+		aircon["now_temp"] = new_temp
+		aircon["last_update"] = new_update
+
+def print_aircon(aircon):
+
+	wind_rate = aircon["wind_rate"]
+	now_temp = aircon["now_temp"]
+	set_temp = aircon["set_temp"]
+	env_temp = aircon["env_temp"]
+
+	text = f'''当前风速{volume_text[wind_rate]}
+当前设置温度 {set_temp} °C
+当前群里温度 {now_temp} °C
+当前环境温度 {env_temp} °C'''.strip()
+
+	return text
