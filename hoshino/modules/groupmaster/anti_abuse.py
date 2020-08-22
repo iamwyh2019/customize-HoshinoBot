42baa8c7dcedc4aace0eeaf93f38658f8de38b18
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-08-22 21:11:45 +0800

    修改了被骂时的图

--- hoshino/modules/groupmaster/anti_abuse.py
+++ hoshino/modules/groupmaster/anti_abuse.py
@@ -61,7 +61,7 @@ async def ban_word(session):
 
 	hoshino.logger.critical(f'Self: {ctx["self_id"]}, Message {ctx["message_id"]} from {msg_from}: {ctx["message"]}')
 	hoshino.priv.set_block_user(user_id, timedelta(hours=8))
-	pic = R.img(f"angry{random.randint(1, 3)}.png").cqcode
+	pic = R.img(f"angry.jpg").cqcode
 	await session.send(f"不理你啦！バーカー\n{pic}", at_sender=True)
 	await util.silence(session.ctx, 8*60*60)
 
