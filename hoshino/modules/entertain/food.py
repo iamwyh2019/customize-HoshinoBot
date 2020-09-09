d9e158800a35143762f164dd6d468b2e93d49188
iamwyh2019 <51950361+iamwyh2019@users.noreply.github.com>
2020-09-09 22:57:23 +0800

    减小食物图的重复率
    现在食物图轮完一圈之前都不会重复了。
    代价是放新图进去后要显式地让机器人刷新一下。

--- hoshino/modules/entertain/food.py
+++ hoshino/modules/entertain/food.py
@@ -17,20 +17,34 @@ res_path = path.abspath(path.join(path.expanduser(hoshino.config.RES_DIR),'food'
 if not path.exists(res_path):
 	os.makedirs(res_path)
 
+res_dir = os.listdir(res_path)
+random.shuffle(res_dir)
+res_idx = 0
+
 @sv.on_fullmatch(('来点好看的','来点好康的'))
 async def send_food_pic(bot,event):
-	global res_path
+	global res_dir,res_idx
 
 	now = datetime.now()
 	if END<=now.hour<START:
 		await bot.send(event,f'这个功能只在{START}点到{END}点开放哟', at_sender=True)
 		return
 
-	res_list = os.listdir(res_path)
-	if len(res_list)==0:
+	if len(res_dir)==0:
 		await bot.send(event,'没什么好看的，快去睡觉！',at_sender=True)
 		return
 
-	pic = random.choice(res_list)
+	pic = res_dir[res_idx]
+	res_idx = (res_idx+1) % len(res_dir)
 	pic_path = path.join(res_path,pic)
 	await bot.send(event,MessageSegment.image(f'file:///{pic_path}'))
+
+@sv.on_fullmatch(('刷新好看的','刷新好康的'))
+async def renew_food_pic(bot,event):
+	global res_dir,res_idx
+
+	res_dir = os.listdir(res_path)
+	random.shuffle(res_dir)
+	res_idx = 0
+
+	await bot.send(event,f'刷新成功，现在有{len(res_dir)}张好看的~')
